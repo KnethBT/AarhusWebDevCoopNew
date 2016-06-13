6fdbfed2fd19430e85d7667818c272f2bdaa1093
kennyt <kenneth2503@hotmail.com>
2016-06-13 15:29:10 +0200

    Opsætning af SourceTree

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Net.Mail;
+using System.Text;
+
+using AarhusWebDevCoop.Models;
+using System.Web.Mvc;
+
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult ContactForm()
+        {
+            var model = new ContactModel();
+            return PartialView("ContactForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Comment");
+
+                comment.SetValue("name", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("message", model.Message);
+
+                Services.ContentService.Save(comment);
+
+                var sb = new StringBuilder();
+                sb.AppendFormat("<p>Message: {0}</p>", model.Message);
+                sb.AppendFormat("<p>Subject: {0}</p>", model.Subject);
+                sb.AppendFormat("<p>Email: {0}</p>", model.Email);
+
+                var message = new MailMessage();
+
+                //message.To.Add(new MailAddress(ConfigurationManager.AppSettings["ContactAddress"]));
+                message.Subject = model.Subject;
+                message.From = new MailAddress(model.Email, model.Name);
+                message.Body = sb.ToString();
+                message.IsBodyHtml = true;
+                TempData["success"] = true;
+
+
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AarhusWebDevCoop.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "the enterd emailadress is not valid")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
